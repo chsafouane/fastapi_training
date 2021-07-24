f27692cebb28e567c0cfbf1cb56790a3341fe846
Safouane Chergui <chsafouane@gmail.com>
2021-07-24 20:53:12 +0200

    [MODIF] main.py - using specific responses

--- ch03/main.py
+++ ch03/main.py
@@ -8,11 +8,11 @@ api = fastapi.FastAPI()
 @api.get("/api/calculate")
 def calculate(x: int, y: int, z: Optional[int] = None):
     if z == 0:
-        return fastapi.Response(
-            content='{"error":"Error: z cannot be zero"}',
-            media_type="application/json",
-            status_code=400
+        return fastapi.responses.JSONResponse(
+            content = {"error": "Error: z cannot be zero"},
+            status_code = 400
         )
+
 
     value = x + y
     if z is not None:
